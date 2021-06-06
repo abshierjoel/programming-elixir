0bc15d1f4d60eac7e25225fa616f06593f8968ae
abshierjoel <abshierjoel@gmail.com>
2021-06-06 14:54:03 -0500

    Add quintuple

--- ch6/times.exs
+++ ch6/times.exs
@@ -2,4 +2,5 @@ defmodule Times do
   def double(n), do: n * 2
   def triple(n), do: n * 3
   def quadruple(n), do: n |> double |> double
+  def quintuple(n), do: n |> double |> double |> double
 end
