d9b84f9b49ce81d0f3922c270fd3817c2ba54c23
Joel Abshier <JAbshier@midwayusa.com>
2021-07-14 15:41:39 -0500

    Add Strings and BInaries Exercise 1

---
+++ lib/ch11/StringsAndBinaries-1.ex
@@ -0,0 +1,10 @@
+defmodule StringZone do
+  def can_print?(str), do: is_ascii(str)
+
+  defp is_ascii([]), do: true
+  defp is_ascii([head | tail]) when head > 32 and head < 127, do: is_ascii(tail)
+  defp is_ascii([_head | _tail]), do: false
+
+  # def anagram?(word1, word2) do
+  # end
+end
